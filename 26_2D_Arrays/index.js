29eda52af629e6a90f799b16c3e5177ff4672d8d
Arvin O. Paguio <apaguio.ai@gmail.com>
2026-09-14 12:02:40 +0800

    new folder: 26_2D_Arrays

---
+++ 26_2D_Arrays/index.js
@@ -0,0 +1,17 @@
+// 2D array = multi-dimensional array that stores a matrix
+//            of data in rows and columns.
+//            Useful for games, spreadsheets, or representing images
+
+//            [X, O, X]
+//            [O, X, X]
+//            [X, O, X]
+
+const matrix = [[1, 2, 3],
+                [4, 5, 6],
+                [7, 8, 9],
+                ['*', 0, '#']];
+
+for(let row of matrix){
+    const rowString = row.join(' ');
+    console.log(rowString);
+}
