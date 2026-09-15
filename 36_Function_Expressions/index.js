8439f957fdc13d8007c8b7b084a7d4855ab07ce0
Arvin O. Paguio <apaguio.ai@gmail.com>
2026-09-15 11:29:27 +0800

    new folder: 36_Function_Expressions

---
+++ 36_Function_Expressions/index.js
@@ -0,0 +1,18 @@
+// function expressions = a way to define functions as
+//                        values or variables
+
+// 1. Callbacks in asychronous operations
+// 2. Higher-Order Functions
+// 3. Closures
+// 4. Event Listeners
+
+const hello = function(){
+    console.log("Hello");
+}
+
+setTimeout(function(){
+    console.log("Goodbye");
+}, 5000);
+
+
+
