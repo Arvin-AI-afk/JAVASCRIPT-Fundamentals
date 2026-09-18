2d6a13b20464fd971420f5fbad5a23936f1762c1
Arvin O. Paguio <apaguio.ai@gmail.com>
2026-09-18 14:17:58 +0800

    new folder: 43_Inheritance

---
+++ 43_Inheritance/index.js
@@ -0,0 +1,44 @@
+// inheritance = allows a new class to inherit properties and methods
+//               from an existing class (parent -> child)
+//               helps with code reusability
+
+class Animal{
+    live = true;
+
+    eat(){
+        console.log(`This ${this.name} is eating`);
+    }
+    sleep(){
+        console.log(`This ${this.name} is sleeping`);
+    }
+}
+class Rabbit extends Animal{
+    name = "rabbit";
+
+    run(){
+        console.log(`This ${this.name} is running`);
+    }
+}
+class Fish extends Animal{
+    name = "Fish";
+
+    swim(){
+        console.log(`This ${this.name} is swimming`);
+    }
+}
+class Hawk extends Animal{
+    name = "Hawk";
+
+    fly(){
+        console.log(`This ${this.name} is flying`);
+    }
+}
+
+const rabbit = new Rabbit();
+const fish = new Fish();
+const hawk = new Hawk();
+
+console.log(hawk.live);
+hawk.eat();
+hawk.sleep();
+hawk.fly();
