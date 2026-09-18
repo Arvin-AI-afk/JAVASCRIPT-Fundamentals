dc0918cfe60fe74d3a2b6d309479b25b39c3ed63
Arvin O. Paguio <apaguio.ai@gmail.com>
2026-09-18 16:40:12 +0800

    new folder: 47_Nested_Objects

---
+++ 47_Nested_Objects/index.js
@@ -0,0 +1,32 @@
+
+class Person{
+
+    constructor(name, age, ...address){
+        this.name = name;
+        this.age = age;
+        this.address = new Address(...address);
+    }
+}
+
+class Address{
+
+    constructor(street, city, country){
+        this.street = street;
+        this.city = city;
+        this.country = country;
+    }
+}
+
+const person1 = new Person("Spongebob", 30, "124 Conch St.",
+                                            "Bikini Bottom",
+                                            "Int. Waters");
+
+const person2 = new Person("Patrick", 37, "128 Conch St.",
+                                            "Bikini Bottom",
+                                            "Int. Waters");
+
+const person3 = new Person("Patrick", 37, "126 Conch St.",
+                                            "Bikini Bottom",
+                                            "Int. Waters");
+
+console.log(person3.address.country);
